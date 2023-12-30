ac70f484469e9a638bb12d8ec149247617193086
eduvarjaor <eduardovarjaor@gmail.com>
2023-12-29 22:06:22 -0300

    feat(Header): added connection with metamask

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,20 +1,64 @@
+'use client';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import metamask from '/public/metamask.png';
 
+declare global {
+    interface Window {
+        ethereum?: {
+            request: (request: {
+                method: string;
+                params?: any[];
+            }) => Promise<any>;
+        };
+    }
+}
+
 function Header() {
+    const [isWalletConnected, setIsWalletConnected] = useState(false);
+
+    async function connectWallet(): Promise<void> {
+        if (window.ethereum) {
+            try {
+                const accounts = await window.ethereum.request({
+                    method: 'eth_requestAccounts',
+                });
+
+                const account = accounts[0];
+                setIsWalletConnected(true);
+            } catch (error) {
+                console.error('Error connecting to MetaMask', error);
+            }
+        } else {
+            alert('Please install MetaMask!');
+        }
+    }
+
     return (
         <div className="bg-zinc-900 w-full flex justify-between p-[1rem]">
             <span className="text-2xl p-2 text-white font-bold">
                 Decentral Home
             </span>
 
-            <button className="bg-neutral-300 hover:bg-neutral-200 duration-200 ease-in-out p-3 rounded-xl flex text-md shadow-lg">
-                <Image
-                    src={metamask}
-                    alt="metamask-logo"
-                    className="w-6 h-6 mr-2"
-                />
-                Connect your wallet
+            <button
+                onClick={connectWallet}
+                disabled={isWalletConnected}
+                className={`bg-neutral-300 hover:bg-neutral-200 duration-200 ease-in-out p-3 rounded-xl flex text-md shadow-lg ${
+                    isWalletConnected ? 'opacity-90 cursor-not-allowed' : ''
+                }`}
+            >
+                {!isWalletConnected ? (
+                    <>
+                        <Image
+                            src={metamask}
+                            alt="metamask-logo"
+                            className="w-6 h-6 mr-2"
+                        />
+                        Connect your wallet
+                    </>
+                ) : (
+                    'Connected'
+                )}
             </button>
         </div>
     );
