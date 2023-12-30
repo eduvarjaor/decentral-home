cf1b0e3504b32d459237dc123417e551c6b96719
eduvarjaor <eduardovarjaor@gmail.com>
2023-12-30 18:13:18 -0300

    feat(properties.ts): separate from Properties.tsx

---
+++ client/src/utils/properties.ts
@@ -0,0 +1,35 @@
+import apartament1 from '/public/apartament1.jpg';
+import apartament2 from '/public/apartament2.jpg';
+import apartament3 from '/public/apartament3.jpg';
+import apartament4 from '/public/apartament4.jpg';
+
+export const properties = [
+    {
+        id: 1,
+        title: 'Panoramic City Views',
+        description:
+            'Airy 2-bedroom with ample natural light, in the city center.',
+        image: apartament1,
+    },
+    {
+        id: 2,
+        title: 'Minimalist Urban Space',
+        description:
+            'Sleek 2-bedroom with modern design and integrated kitchen.',
+        image: apartament2,
+    },
+    {
+        id: 3,
+        title: 'Luxury Urban Living',
+        description:
+            '2-bedroom combining luxury with a high-standard urban lifestyle.',
+        image: apartament3,
+    },
+    {
+        id: 4,
+        title: 'Charming and Modern',
+        description:
+            'Cozy 2-bedroom with stylish wood accents and a homely feel.',
+        image: apartament4,
+    },
+];
