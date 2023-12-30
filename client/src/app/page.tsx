941d84e1b3296839d1b253ef3a62c0c2fa2fe252
eduvarjaor <eduardovarjaor@gmail.com>
2023-12-30 18:10:05 -0300

    feat(page): add id for main tag

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,7 +2,7 @@ import App from '../components/App';
 
 export default function Home() {
     return (
-        <main className="flex flex-col min-h-screen">
+        <main className="flex flex-col min-h-screen" id="page">
             <App />
         </main>
     );
