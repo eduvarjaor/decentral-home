38a2ca6c30ee2477275aee2c708cc5ea22dce469
eduvarjaor <eduardovarjaor@gmail.com>
2023-12-30 18:37:09 -0300

    feat(App): use client and pass connection wallet as props

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,18 @@
+'use client';
+import React, { useState } from 'react';
 import Header from './Header';
 import Properties from './Properties';
 
 function App() {
+    const [isWalletConnected, setIsWalletConnected] = useState(false);
+
     return (
         <>
-            <Header />
-            <Properties />
+            <Header
+                isWalletConnected={isWalletConnected}
+                setIsWalletConnected={setIsWalletConnected}
+            />
+            <Properties isWalletConnected={isWalletConnected} />
         </>
     );
 }
