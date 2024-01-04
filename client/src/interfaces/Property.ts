faaedd89d3dec417e74fb53ae7067f0b00851f73
eduvarjaor <eduardovarjaor@gmail.com>
2024-01-04 18:49:04 -0300

    feat(Property): export interface

---
+++ client/src/interfaces/Property.ts
@@ -0,0 +1,9 @@
+import { StaticImageData } from 'next/image';
+
+export interface Property {
+    id: number;
+    title: string;
+    description: string;
+    image: StaticImageData;
+    isRented: boolean;
+}
