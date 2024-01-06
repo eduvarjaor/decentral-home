9f180024a920189558b0f420d43e42055b7d64fc
eduvarjaor <eduardovarjaor@gmail.com>
2024-01-06 16:18:02 -0300

    fix(truffle-config): new providerOrUrl

--- smart_contracts/truffle-config.js
+++ smart_contracts/truffle-config.js
@@ -7,23 +7,29 @@ module.exports = {
         bscscan: process.env.BSCSCAN_API_KEY,
     },
     networks: {
-        bsctest: {
-            network_id: "97",
-            provider: () =>
-                new HDWalletProvider({
-                    mnemonic: {
-                        phrase: process.env.MNEMONIC,
-                    },
-                    providerOrUrl:
-                        "https://data-seed-prebsc-1-s1.bnbchain.org:8545",
-                }),
-            gasPrice: 10000000000,
-            confirmations: 10,
-            timeoutBlocks: 200,
-
-            skipDryRun: true,
+        development: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*", // Match any network id
         },
     },
+    // networks: {
+    //     bsctest: {
+    //         network_id: "97",
+    //         provider: () =>
+    //             new HDWalletProvider({
+    //                 mnemonic: {
+    //                     phrase: process.env.MNEMONIC,
+    //                 },
+    //                 providerOrUrl: "https://bsc-testnet.publicnode.com",
+    //             }),
+    //         gas: 2100000,
+    //         gasPrice: 8000000000,
+    //         networkCheckTimeout: 10000,
+    //         timeoutBlocks: 200,
+    //         skipDryRun: true,
+    //     },
+    // },
     compilers: {
         solc: {
             version: "0.8.0",
